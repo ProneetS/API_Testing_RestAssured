b6d7838643f6b6a903197f1caf898e92cdc445b4
ProneetS <proneetseth.office@gmail.com>
2024-03-28 22:53:16 +0530

    Calling the payload JSON inside body() by calling Package files.

--- Project1_API_Testing/src/BasicAPITest.java
+++ Project1_API_Testing/src/BasicAPITest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import jsonData.Payload;
 
 import static io.restassured.RestAssured.*; //for given()
 
@@ -16,22 +17,7 @@ public class BasicAPITest {
 		//step-1: Setting up base URL 
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParam("key","qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}")
+		.body(Payload.AddPlace())
 		.when().post("maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)");
 		
