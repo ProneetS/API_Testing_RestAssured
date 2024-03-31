d435bc6ae20382a40e0fa69f6f40f325dfdabe6b
ProneetS <proneetseth.office@gmail.com>
2024-03-31 08:02:34 +0530

    ReusableMethods to Simplify code. JsonPath will convert String JSON

---
+++ Project1_API_Testing/src/jsonData/ReusableMethods.java
@@ -0,0 +1,11 @@
+package jsonData;
+
+import io.restassured.path.json.JsonPath;
+
+public class ReusableMethods {
+	public static JsonPath rawToJson(String response) {
+		JsonPath js1 = new JsonPath(response);
+		return js1;
+		
+	}
+}
