cda759e89727295e6c480741d3b83c6ea3914c89
ProneetS <proneetseth.office@gmail.com>
2024-04-02 13:03:46 +0530

    Retrieving data from JSONPath from dummy API

--- Project1_API_Testing/src/ComplexJson.java
+++ Project1_API_Testing/src/ComplexJson.java
@@ -1,8 +1,18 @@
+import io.restassured.path.json.JsonPath;
+import jsonData_Payload.Payload;
 
 public class ComplexJson {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JsonPath js2 = new JsonPath(Payload.CoursePrice()); //Here the response we are not getting from any API 
+		
+		//Retrieving the JSON Array size and its element using JsonPath
+		//Q1 - 1.	Print Number of courses returned by API
+		//Ans1 - js2.getInt("courses.size()"); method used
+		int count = js2.getInt("courses.size()");
+		System.out.println(count);
 
 	}
 
